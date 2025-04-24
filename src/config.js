15b3ce069029c9a6d7bac9d5c9911de1fec723f6
bhaasobh <31213499+bhaasobh@users.noreply.github.com>
2025-04-24 22:42:21 +0300

    added my list row

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ const config = {
     Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkYTEwYzM2Y2EwMTRiYTY3NTFiMjc1MmUwZWI1MDVmMiIsIm5iZiI6MTc0NDUzNDc3MC4yMDUsInN1YiI6IjY3ZmI3Y2YyYTA0ZjUzMGI1Yzk5N2FmMiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.TCvJfv92k4dz3hll0epCgRu0FfkwGsmne5jbqImeCVE',
     TMDB_API:'https://api.themoviedb.org/3',
     TMDB_IMAGE:'https://image.tmdb.org/t/p/w780',
-     SERVER_API: 'http://192.168.56.1:3001/api'
-    // SERVER_API: 'https://netflixserver-i36m.onrender.com/api'
+     //SERVER_API: 'http://192.168.56.1:3001/api'
+    SERVER_API: 'https://netflixserver-i36m.onrender.com/api'
 
 };
 
